65d21e3109c9634ed0c28ffedf9a284a8649bd34
Drew Tufano <dtufano@hotmail.com>
2014-03-23 10:50:15 -0500

    refactored initial directive and added comments, working

--- js/modules/menu/menu-item-directive.js
+++ js/modules/menu/menu-item-directive.js
@@ -7,10 +7,20 @@ define(['./module'], function (ng) {
 
  	ng.directive('menuItem', function () {
 		return {
+		/**
+		 * Restrict to the following element
+		 * A - attribute, E - element, C - class, M - comment
+		 * Can combine ie AEC would look for attribute, element, and class
+		 */
 	  	restrict : 'A',
 	  	replace : true,
 	  	templateUrl : '/js/modules/menu/item.html',
-	  	transclude: true,
+	  	// create isolated scope with the following (=attr) example <div bind-to-this="thing"></div> =bind-to-this
+	  	scope: {
+	  		id : '=id',
+	  		name : '=name',
+	  		url : '=url'
+	  	},
 	  	//TODO: define directive controller.
 	  	link : function(scope, element, attrs) {
 	    },
